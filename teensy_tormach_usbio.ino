48994ed8c1d8898cb0309036cb1f96a75df1c1aa
Jon Watte <jwatte@gmail.com>
2019-01-11 20:35:26 -0800

    Intermittent-duty relay control

--- teensy_tormach_usbio.ino
+++ teensy_tormach_usbio.ino
@@ -1,40 +1,18 @@
 
-//  define ON_ARDUINO to 1 to make work with Arduino Uno
-#define ON_ARDUINO 0
-#if !ON_ARDUINO
-#define USE_TOUCH 1
-#define TOUCH_DELTA 300
-#endif
-
-#if ON_ARDUINO
-#define INPUT0 A0
-#define INPUT1 A1
-#define INPUT2 A2
-#define INPUT3 A3
-
-#define OUTPUT0 2
-#define OUTPUT1 3
-#define OUTPUT2 4
-#define OUTPUT3 5
-
-#define MSG_PIN 13
-#else
-#define INPUT0 15
-#define INPUT1 16
-#define INPUT2 17
-#define INPUT3 19
+/* This sketch runs on Teensy LC.
+ *  It drives the same relay (pin 20) with four different duty cycles
+ *  based on which output relay is on.
+ *  P3 -- 3 seconds every 16 seconds.
+ *  P2 -- 2 seconds every 8 seconds
+ *  P1 -- 1 second every 4 seconds
+ *  P0 -- continually on
+ */
 
 #define OUTPUT0 20
-#define OUTPUT1 21
-#define OUTPUT2 22
-#define OUTPUT3 23
-
 #define MSG_PIN 13
-#endif
 
 
 #define VERSION_STR "Teensy for Tormach I/O ID=0\n"
-#define DEBOUNCE_MS 10
 
 //  what to actually set the outputs to
 bool outputs[4];
@@ -46,84 +24,16 @@ bool tInputs[4];
 //  time to use for de-bounce
 uint32_t tmInputs[4];
 
-#if USE_TOUCH
-class TouchPin {
-  public:
-  TouchPin(uint8_t pin) : pin_(pin), on_(false), low_(800), high_(1300), last_(500) {
-  }
-  bool read() {
-    uint16_t rd = touchRead(pin_);
-    last_ = rd;
-    if (on_) {
-      if (rd < high_) {
-        on_ = false;
-      } else {
-        high_ = (rd > TOUCH_DELTA) ? rd - TOUCH_DELTA : 0;
-        if (high_ < low_ + TOUCH_DELTA) {
-          high_ = low_ + TOUCH_DELTA;
-        }
-        if (high_ > low_ * 4) {
-          high_ = low_ * 4;
-        }
-      }
-    } else {
-      if (rd > low_ + TOUCH_DELTA) {
-        on_ = true;
-      } else {
-        low_ = rd;
-        if (low_ > high_ - TOUCH_DELTA) {
-          low_ = high_ - TOUCH_DELTA;
-        }
-        if (low_ > high_ / 2) {
-          low_ = high_ / 2;
-        }
-      }
-    }
-    return on_;
-  }
-  char const *fmt() {
-    sprintf(buf_, "%d/%d/%d", low_, high_, last_);
-    return buf_;
-  }
-  char buf_[24];
-  uint8_t pin_;
-  bool on_;
-  uint16_t low_;
-  uint16_t high_;
-  uint16_t last_;
-};
-
-TouchPin touchPin0(INPUT0);
-TouchPin touchPin1(INPUT1);
-TouchPin touchPin2(INPUT2);
-TouchPin touchPin3(INPUT3);
-#endif
-
 void setup() {
-#if ON_ARDUINO
-  Serial.begin(38400);
-#endif
   // put your setup code here, to run once:
   pinMode(OUTPUT0, OUTPUT);
   digitalWrite(OUTPUT0, LOW);
-  pinMode(OUTPUT1, OUTPUT);
-  digitalWrite(OUTPUT1, LOW);
-  pinMode(OUTPUT2, OUTPUT);
-  digitalWrite(OUTPUT2, LOW);
-  pinMode(OUTPUT3, OUTPUT);
-  digitalWrite(OUTPUT3, LOW);
-  pinMode(MSG_PIN, OUTPUT);
-  digitalWrite(MSG_PIN, LOW);
-
-  pinMode(INPUT0, INPUT);
-  pinMode(INPUT1, INPUT);
-  pinMode(INPUT2, INPUT);
-  pinMode(INPUT3, INPUT);
 }
 
 char inbuf[256];
 uint8_t iptr;
 bool led;
+uint32_t lastcmdtime;
 
 void toggleLed() {
   led = !led;
@@ -139,6 +49,7 @@ void loop() {
   while (n > 0) {
     --n;
     gotCmd = true;
+    lastcmdtime = now;
     int ch = Serial.read();
     if (ch == '\r' || ch == '\n') {
       if (iptr > 0) {
@@ -166,15 +77,9 @@ void loop() {
         } else if (inbuf[0] == '?') {
           inbuf[iptr] = 0;
           char buf[120];
-#if ON_ARDUINO
           sprintf(buf, "* %d%d%d%d %d%d%d%d\n",
             outputs[0], outputs[1], outputs[2], outputs[3],
             inputs[0], inputs[1], inputs[2], inputs[3]);
-#else
-          sprintf(buf, "* %s %s %s %s %d%d%d%d\n",
-            touchPin0.fmt(), touchPin1.fmt(), touchPin2.fmt(), touchPin3.fmt(),
-            inputs[0], inputs[1], inputs[2], inputs[3]);
-#endif
           Serial.write(buf);
         } else {
           //  bad CMD!!
@@ -192,41 +97,20 @@ void loop() {
     }
   }
 
-#if USE_TOUCH
-  tInputs[0] = touchPin0.read();
-  tInputs[1] = touchPin1.read();
-  tInputs[2] = touchPin2.read();
-  tInputs[3] = touchPin3.read();
-#else
-  tInputs[0] = digitalRead(INPUT0);
-  tInputs[1] = digitalRead(INPUT1);
-  tInputs[2] = digitalRead(INPUT2);
-  tInputs[3] = digitalRead(INPUT3);
-#endif
-
-  //  debounce input pins
-  for (int i = 0; i != 4; ++i) {
-    if (tInputs[i] == inputs[i]) {
-      tmInputs[i] = now;
-    }
-    if (now - tmInputs[i] > DEBOUNCE_MS) {
-      inputs[i] = tInputs[i];
-    }
+  //  turn off if the computer goes away and stops sending
+  if (uint32_t(now - lastcmdtime) > 5000) {
+    memset(outputs, 0, sizeof(outputs));
+    lastcmdtime = now;
   }
-  
+
   //  output relay status
-  digitalWrite(OUTPUT0, outputs[0]);
-  digitalWrite(OUTPUT1, outputs[1]);
-  digitalWrite(OUTPUT2, outputs[2]);
-  digitalWrite(OUTPUT3, outputs[3]);
+  digitalWrite(OUTPUT0,
+    (outputs[3] && ((now & 16383) < 3000)) ||
+    (outputs[2] && ((now & 8191) < 2000)) ||
+    (outputs[1] && ((now & 4095) < 1000)) ||
+    outputs[0]
+    );
 
-#if !USE_TOUCH
-  //  Touch reading takes time, but if not using touch, no need to be too fast
-  if (!gotCmd) {
-    delay(1);
-  }
-#else
-  (void)&gotCmd;
-#endif
+  delay(1);
 }
 
